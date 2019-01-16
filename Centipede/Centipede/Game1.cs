955376d848263e873dffd5b2f18035af7667f202
PatrickDrury <46572411+PatrickDrury@users.noreply.github.com>
2019-01-16 11:47:35 -0600

    making space

--- Centipede/Centipede/Game1.cs
+++ Centipede/Centipede/Game1.cs
@@ -120,6 +120,17 @@ namespace Centipede
             if (kb.IsKeyDown(Keys.LeftAlt) && !kbO.IsKeyDown(Keys.LeftAlt))
                 restart();//This is to test what you are working on in multiple levels. (Secret skip button)
 
+
+
+
+
+
+
+
+
+
+
+
             //player movement logic
             if (kb.IsKeyDown(Keys.W))
             {
