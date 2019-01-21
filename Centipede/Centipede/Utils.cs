afd1089d0b364f585ee0ab9622e188d3f714e022
Ceranth <will@stickpros.com>
2019-01-21 15:25:33 -0600

    Added different colors for the global colors array

--- Centipede/Centipede/Utils.cs
+++ Centipede/Centipede/Utils.cs
@@ -17,7 +17,7 @@ namespace Centipede {
         //
         // This list is executed in order from 0 -> last element. Once last element has been displayed the
         // list is restarted starting from 0.
-        public static readonly Color[] backgroundColors = { Color.Black };
+        public static readonly Color[] backgroundColors = { Color.Green, Color.Pink, Color.Red, Color.Yellow };
 
         // Global random state
         public static Random rng = new Random();
